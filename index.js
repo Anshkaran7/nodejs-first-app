28bc74007f0291af1ca7383ebac03ae9767228bd
Karan <karanchandravanshi217@gmail.com>
2023-04-15 14:05:04 +0530

    Day12(Vacation)

--- index.js
+++ index.js
@@ -2,15 +2,36 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const users = [];
+
+//using middlewares
 app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 // console.log(path.join(path.resolve(), "public"))
 
+//setting up view engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index", { name: "Karan" });
 });
 
+app.get("/success", (req, res) => {
+  res.render("success");
+});
+
+app.post("/contact", (req, res) => {
+  // console.log(req.body);
+  // console.log(req.body.name);
+  users.push({ username: req.body.name, email: req.body.email });
+  res.redirect("/success");
+});
+app.get("/users", (req,res)=>{
+  res.json({
+    users
+  }
+  )
+})
 app.listen(5000, () => {
   console.log("Server is running");
 });
